11324e008d6c5ed440c3230b9d3c7ea953e0339e
“Your <koltonthompson@koltons-MacBook-Pro.local>
2021-03-01 21:22:11 -0700

    testing 1-2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn how to code you pants 
+          Learn how to do mini projects
         </a>
       </header>
     </div>
